d1b3f6cc96c5bc2355393ae4bd39b1beba73f351
Phoenix <59854163+rahm-0x@users.noreply.github.com>
2024-09-12 05:37:38 +0800

    added typesense filters to streamlit main

--- oldstreamlit.py
+++ oldstreamlit.py
@@ -1,25 +1,14 @@
-#streamlit_app.py
 import streamlit as st
 import pandas as pd
-from pandas import DataFrame 
+from pandas import DataFrame
 import psycopg2
 from sqlalchemy import create_engine
 import base64
-import _thread
 import hashlib
 import plotly.express as px
 
 # Set the page layout
 st.set_page_config(layout='wide')
-
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-    b64 = base64.b64encode(data).decode()
-    md = f"""<audio controls autoplay="true">
-              <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-              </audio>"""
-    st.markdown(md, unsafe_allow_html=True)
 
 # Local cache dictionary
 local_cache = {}
@@ -29,18 +18,11 @@ def hash_credentials(credentials):
     return hashlib.md5(cred_str.encode()).hexdigest()
 
 def calculate_sales(row: DataFrame, date_cols: list):
-    # Reversing the date columns since the latest date is first
     reversed_date_cols = date_cols[::-1]
-    
-    # Calculate daily differences; the diff operation goes 'backwards' because of the reversed column order
-    diffs = row[reversed_date_cols].diff(-1).fillna(0)
-    
-    # Remove negative sales because they might indicate returns or inventory adjustments
+    diffs = row[reversed_date_cols].diff(-1).fillna(0).infer_objects()
     diffs = diffs[diffs >= 0]
-    
-    # Sum up the positive sales to get the total sales for the given date range
     return diffs.sum()
-    
+
 def fetch_inventory_data(query):
     credentials = {
         'dbname': st.secrets["dbname"],
@@ -53,8 +35,13 @@ def fetch_inventory_data(query):
     cache_key = f"{cred_hash}:{hashlib.md5(query.encode()).hexdigest()}"
     
     if cache_key not in local_cache:
-        engine = create_engine(f"postgresql+psycopg2://{credentials['user']}:{credentials['password']}@{credentials['host']}:{credentials['port']}/{credentials['dbname']}")
-        local_cache[cache_key] = pd.read_sql(query, engine)
+        try:
+            engine = create_engine(f"postgresql+psycopg2://{credentials['user']}:{credentials['password']}@{credentials['host']}:{credentials['port']}/{credentials['dbname']}")
+            with engine.connect() as connection:
+                local_cache[cache_key] = pd.read_sql(query, connection)
+        except Exception as e:
+            st.error(f"Error executing query: {e}")
+            return pd.DataFrame()  # Return an empty DataFrame on error
 
     return local_cache[cache_key]
 
@@ -82,6 +69,20 @@ with main_container:
         query = "SELECT * FROM public.t_dynamic_inventory_reverseddates WHERE brand like 'The Grower Circle'"
         df = fetch_inventory_data(query)
         st.session_state['df'] = df  # Store df in session state
+    elif st.button("Fetch Typesense Data"):
+        query = 'SELECT * FROM public.typesense_table'
+        df_typesense = fetch_inventory_data(query)
+        st.session_state['typesense_df'] = df_typesense
+    elif st.button("Fetch TGC from Typesense"):
+        query = """
+        SELECT * FROM public.typesense_table
+        WHERE "Brand" ILIKE '%flight%'
+           OR "Brand" ILIKE '%Grower Circle%'
+           OR "Brand" ILIKE '%Growers Circle%'
+           OR "Brand" ILIKE '%GROWERS CIRCLE%'
+        """
+        df_typesense_tgc = fetch_inventory_data(query)
+        st.session_state['typesense_df'] = df_typesense_tgc
 
     if 'df' in st.session_state:
         df = st.session_state['df']  # Retrieve df from session state
@@ -90,28 +91,25 @@ with main_container:
         # Handle null values by replacing them with 0
         df[recent_dates] = df[recent_dates].fillna(0)
     
-        # Assuming 'recent_dates' holds the sorted date columns with the latest date first
         if recent_dates:  # Check if recent_dates list is not empty
             latest_date = recent_dates[0]
         else:
             st.warning("No recent dates available.")
-            latest_date = None  # Set latest_date to None if no recent dates are available
+            latest_date = None
 
         # Calculate sales since yesterday
         if len(recent_dates) >= 2:
             df['Sales_Since_Yesterday'] = df[recent_dates[1]] - df[recent_dates[0]]
         else:
-            df['Sales_Since_Yesterday'] = "NA"  # Fill with "NA" if data is insufficient
+            df['Sales_Since_Yesterday'] = "NA"
 
-        # Convert 'Sales_Since_Yesterday' column to numeric type
         df['Sales_Since_Yesterday'] = pd.to_numeric(df['Sales_Since_Yesterday'], errors='coerce')
 
         # Filter out non-numeric values and then find the largest 5 values
         top_brands = df.groupby('name')['Sales_Since_Yesterday'].sum().dropna().nlargest(5)
 
-       # Move 'Sales_Since_Yesterday' after 'price' column and before the first date column
         if 'Sales_Since_Yesterday' in df.columns:
-            price_col_index = df.columns.get_loc("price") + 1  # Get index of 'price' column and add 1 to move after
+            price_col_index = df.columns.get_loc("price") + 1
             cols = df.columns.tolist()
             cols.insert(price_col_index, cols.pop(cols.index('Sales_Since_Yesterday')))
             df = df[cols]
@@ -121,14 +119,13 @@ with main_container:
         filter_options = update_filter_options(df)
         
         if latest_date is not None:
-            df_in_stock = df[df[latest_date] > 0]  # Products in stock
-            df_out_of_stock = df[df[latest_date] <= 0]  # Products out of stock or null
+            df_in_stock = df.loc[df[latest_date] > 0]  # Use .loc to avoid the warning
+            df_out_of_stock = df.loc[df[latest_date] <= 0]  # Use .loc to avoid the warning
         else:
-            df_in_stock = pd.DataFrame()  # Empty DataFrame
-            df_out_of_stock = pd.DataFrame()  # Empty DataFrame
+            df_in_stock = pd.DataFrame()
+            df_out_of_stock = pd.DataFrame()
 
 
-        # Filter widgets
         selected_brand = st.selectbox("Filter by Brand", filter_options["brands"], key='brand_filter')
         if selected_brand != "ALL":
             df = df[df['brand'] == selected_brand]
@@ -154,24 +151,16 @@ with main_container:
         if search_term:
             df = df[df.apply(lambda row: row.astype(str).str.contains(search_term, case=False).any(), axis=1)]
 
-
         st.title("Dispensary On-Hands & Sales Data")
         
-        
-        # Create two separate DataFrames
         if latest_date is not None:
-            df_in_stock = df[df[latest_date] > 0]  # Products in stock
-            df_out_of_stock = df[df[latest_date] <= 0]  # Products out of stock or null
+            df_in_stock = df[df[latest_date] > 0]
+            df_out_of_stock = df[df[latest_date] <= 0]
         else:
             st.warning("No recent dates available.")
-            df_in_stock = pd.DataFrame()  # Empty DataFrame
-            df_out_of_stock = pd.DataFrame()  # Empty DataFrame
+            df_in_stock = pd.DataFrame()
+            df_out_of_stock = pd.DataFrame()
 
-
-        df_out_of_stock = df[df[latest_date] <= 0] if latest_date is not None else pd.DataFrame()
-
-        
-        # Display these DataFrames as separate tables in Streamlit
         st.header("Products In Stock")
         st.dataframe(df_in_stock)
         
@@ -180,10 +169,8 @@ with main_container:
 
         st.markdown("* Sales since yesterday will be inaccurate for many dispensaries that cap the inventory levels in the data")
 
-        # Dashboard Section
         st.title("Top Selling Products Dashboard")
 
-        # Sales Calculation
         last_3_days = recent_dates[:3]
         last_7_days = recent_dates[:7]
         last_30_days = recent_dates[:30]
@@ -192,7 +179,6 @@ with main_container:
         df['Sales_Last_7_Days'] = df.apply(lambda row: calculate_sales(row, last_7_days), axis=1)
         df['Sales_Last_30_Days'] = df.apply(lambda row: calculate_sales(row, last_30_days), axis=1)
 
-        # Dashboard Columns
         col1, col2 = st.columns(2)
 
         with col1:
@@ -215,10 +201,20 @@ with main_container:
             st.subheader("Last 30 Days")
             st.dataframe(top_sold_30_days)
 
-        
             top_brands = df.groupby('name')['Sales_Since_Yesterday'].sum().nlargest(5)
             fig = px.pie(values=top_brands.values, names=top_brands.index, title='Top 5 Items by Sales Since Yesterday', labels={'value': 'Sales Since Yesterday'})
             fig.update_traces(textinfo='label+value')
             st.plotly_chart(fig, use_container_width=True)
+
+    if 'curaleaf_df' in st.session_state:
+        curaleaf_df = st.session_state['curaleaf_df']
+        st.header("Curaleaf Data")
+        st.dataframe(curaleaf_df)
+
+    if 'typesense_df' in st.session_state:
+        typesense_df = st.session_state['typesense_df']
+        st.header("Typesense Data")
+        st.dataframe(typesense_df)
+
     else:
         st.warning("Please fetch data to proceed.")
